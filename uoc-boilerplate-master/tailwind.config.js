9e643451abbec35353b898b38c02f5fe43bb6026
gruguuoc <grugu@uoc.edu>
2025-05-31 18:45:01 +0200

    added @apply directive in order not to duplicate code

--- uoc-boilerplate-master/tailwind.config.js
+++ uoc-boilerplate-master/tailwind.config.js
@@ -4,26 +4,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary':  '#CADBC5',
-        'strong-green':  '#4B534B',
-        'light-grey': '#ced4da'
+        'primary':'#CADBC5',
+        'strong-green':'#4B534B',
+        'light-grey':'#ced4da'
       },
       gap: {
-        'rem3': '3rem', 
+        'rem3':'3rem', 
        },
        maxWidth: {
-        'md-460': '460px', 
+        'md-460':'460px', 
       },
       fontFamily: {
-        archivoBlack: ['"Archivo Black"', 'sans-serif'],
+        archivoBlack:['"Archivo Black"', 'sans-serif'],
       },
       fontSize: {
-        '3xxl': '2rem',
-        '5xxl': '2.8rem',
+        '3xxl':'2rem',
+        '5xxl':'2.8rem',
       },
-    },
-    container: {
-      type: 'inline-size',
     }
   },
   plugins: [
