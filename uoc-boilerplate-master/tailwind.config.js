1be866586001121a90e09ab8f223b8061d5a414a
gruguuoc <grugu@uoc.edu>
2025-05-31 12:06:52 +0200

    working on speakers page

--- uoc-boilerplate-master/tailwind.config.js
+++ uoc-boilerplate-master/tailwind.config.js
@@ -4,13 +4,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary:  '#CADBC5',
-        masterBrand:  '#4B534B'
-      }
+        'primary':  '#CADBC5',
+        'strong-green':  '#4B534B',
+        'light-grey': '#ced4da'
+      },
+      gap: {
+        'rem3': '3rem', 
+       },
+       maxWidth: {
+        'md-460': '460px', 
+      },
+      fontFamily: {
+        archivoBlack: ['"Archivo Black"', 'sans-serif'],
+      },
+      fontSize: {
+        '3xxl': '2rem',
+        '5xxl': '2.8rem',
+      },
     },
     container: {
       type: 'inline-size',
-    },
+    }
   },
-  plugins: []
+  plugins: [
+    require('@tailwindcss/container-queries'),
+  ]
 }
